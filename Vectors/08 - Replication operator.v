5ce3d7d8e9bf850a6dc5d33385113c03d0b36fbb
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2024-01-20 19:09:28 +0530

    Create 08 - Replication operator.v

---
+++ Vectors/08 - Replication operator.v
@@ -0,0 +1,8 @@
+module top_module (
+    input [7:0] in,
+    output [31:0] out );
+     // The replication operator allows repeating a vector and concatenating them together:
+	//{num{vector}}
+    // assign out = { replicate-sign-bit , the-input };
+    assign out={{24{in[7]}},in};
+endmodule
